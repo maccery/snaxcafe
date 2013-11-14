492ec130ace8c1372fbf0b1e657cac9b8721bb49
Tom Macmichael <tommacmichael@me.com>
2013-11-14 23:51:04 +0000

    Scrolling animation added

--- js/maps.js
+++ js/maps.js
@@ -23,3 +23,15 @@ function initialize() {
     });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+        $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+});
