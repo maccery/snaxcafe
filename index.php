44d5613044f0d73b5a0b5f8e72f9c60c7215321d
Tom Macmichael <tommacmichael@me.com>
2013-11-15 16:56:19 +0000

    Bug fixes

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $closed = '<button type="button" class="btn btn-fail btn-lg">CLOSED</button>';
         <meta property="og:description" content="Breakfast delivered to your door 7 days a week in less than 20 minutes anywhere in central Edinburgh. "/>
         <meta property="og:image" content="http://snaxcafe.com/images/facebook.png" />
 
-        <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.ico">
+        <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
 
     </head>
     <body>
@@ -78,7 +78,7 @@ $closed = '<button type="button" class="btn btn-fail btn-lg">CLOSED</button>';
         </div>
         <img src="images/logo.png" class="img-responsive" style="position: absolute; width: 300px; z-index: 11;">
         <div id="welcome" class="carousel slide" data-ride="carousel">
-            <a href="#contact"><?php if(isOpen('west')||isOpen('buccleuch')) { echo "<img src='../images/open.png' id='open'>"; }else{ echo "<img src='../images/closed.png' id='open'>"; } ?></a>
+            <a href="#contact"><?php if(isOpen('west')||isOpen('buccleuch')) { echo "<img src='images/open.png' id='open'>"; }else{ echo "<img src='images/closed.png' id='open'>"; } ?></a>
             <ol class="carousel-indicators">
                 <li data-target="#welcome" data-slide-to="0" class="active"></li>
                 <li data-target="#welcome" data-slide-to="1"></li>
